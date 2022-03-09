1fac83e2c8b9bbe19a8d3c7b12dc673789ebc100
Jon Chastain <chastain.r.jon@gmail.com>
2022-03-08 23:44:04 -0500

    add models for user, thought, and reactions

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const {Schema, model, Types} = require('mongoose')
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid e-mail address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }],
+    friends: [UserSchema]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+})
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+})
+
+
+const User = model('User', UserSchema)
+
+module.exports = User
---
+++ utils/dateFormat.js
@@ -0,0 +1,4 @@
+const formatDate = require("dateformat")
+module.exports = (date) => {
+    return formatDate(date)
+}
